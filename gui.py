247a124f40993da2c73b9bcf121e3da3735001d9
Dragojak <jakobalhasan@gmail.com>
2023-09-09 19:06:32 +0200

    Added game selection menu
    Reads games from local/InterTok

--- gui.py
+++ gui.py
@@ -10,20 +10,25 @@ TIKTOK_LINK = "https://tiktok.com"
 
 
 
+
+
 appdata =  os.path.abspath(os.path.join(os.getenv("LOCALAPPDATA"), os.pardir))
 key_file_path = os.path.join(appdata, r"locallow\DefaultCompany\Tiktok_BallGame\key")
-unity_path = os.path.join(appdata, r"local\DefaultCompany\Tiktok_BallGame\Tiktok.BallGame.exe")
-username_path = os.path.join(appdata, r"locallow\DefaultCompany\Tiktok_BallGame\username")
+unity_path = os.path.join(appdata, r"local\InterTok")
 
+GAMES = [i for i in os.listdir(unity_path)]
+
+
+
+preUser = ""
 preKey = ""
 if os.path.exists(key_file_path):
     with open(key_file_path, "r") as f:
-        preKey = f.read()
+        
+        preKey = key = f.read()
+        preuser = preKey.split("-")[1]
 
-preUser = ""
-if os.path.exists(username_path):
-    with open(username_path, "r") as f:
-        preUser = f.read()
+
 
 
 font = ("Roboto-Bold", 18, "bold")
@@ -31,30 +36,43 @@ inputFont = ("Roboto-Bold", 10, "bold")
 keyFont = ("Roboto-Bold", 8, "bold")
 buttonFont = ("Roboto-Bold", 12, "bold")
 
-layout = [[sg.Column([[sg.Text("Intertok Games Login", font=font, background_color="#1a2835")]], element_justification="center", expand_x=True, background_color="#1a2835")], 
+sg.theme("DarkBlue2")
+
+
+layout = [[sg.Column([[sg.Text("Intertok Games Login", font=font)]], element_justification="center", expand_x=True)], 
           
-          [sg.Text("Username on TikTok:", font=keyFont, justification="left", background_color="#1a2835")],
-          [sg.Input(preUser, font=inputFont, size=(350, 1), expand_x=True, justification="center", key="username_input")],
+          [sg.Text("Key:", font=keyFont, justification="left"), sg.Text("", font=keyFont, key="errorMessage", text_color="red")], 
+          [sg.Input(preKey, font=inputFont, size=(350, 1), expand_x=True, justification="center", key="key_input", border_width=0)], 
           
-          [sg.Text("Key:", font=keyFont, justification="left", background_color="#1a2835"), sg.Text("", font=keyFont, key="errorMessage", text_color="red", background_color="#1a2835")], 
-          [sg.Input(preKey, font=inputFont, size=(350, 1), expand_x=True, justification="center", key="key_input")], 
-          
-          [sg.Column([[sg.Button("Login", font=buttonFont)]], element_justification="center", expand_x=True, background_color="#1a2835")],
-          [sg.Text("\n", background_color="#1a2835")],
+          [sg.Text("Username on TikTok:", font=keyFont, justification="left")],
+          [sg.Text("", font=inputFont, size=(350, 1), expand_x=True, justification="center", key="username_input", background_color="#335267")],
+
+          [sg.Combo(GAMES, size = (200, 1), default_value=GAMES[0], font=inputFont, readonly=True)],
+
+          [sg.Column([[sg.Button("Login", font=buttonFont, border_width=0)]], element_justification="center", expand_x=True, background_color="#1a2835")],
           [sg.Column([[sg.Image(r"discord.png", key="IMAGE_DISCORD", background_color="#1a2835"), sg.Text(" ", size=(30, 1), background_color="#1a2835"), sg.Image(r"tiktok.png", key="IMAGE_TIKTOK", background_color="#1a2835")]], background_color="#1a2835")]]
 
 
 # Create the window
-sg.theme("DarkBlue2")
-window = sg.Window("Intertok Games Login", layout, size=(400, 250), finalize=True)
+
+window = sg.Window("Intertok Games Login", layout, size=(400, 285), finalize=True)
 window[f'IMAGE_DISCORD'].bind("<Button-1>", "")
 window[f'IMAGE_TIKTOK'].bind("<Button-1>", "")
 # Create an event loop
 while True:
-    event, values = window.read()
+
+
+
+
+
+    event, values = window.read(timeout=1)
     # End program if user closes window or
     # presses the OK button
-    print(event)
+
+    if "-" in window["key_input"].get():
+
+        window["username_input"].update(window["key_input"].get().split("-")[1])
+
     if event == "OK" or event == sg.WIN_CLOSED:
         break
     elif event == "IMAGE_DISCORD":
@@ -64,8 +82,7 @@ while True:
     elif event == "Login":
         success = keyauth.checkLicense(str(window["key_input"].get()))
 
-        with open(username_path, "w") as f:
-            f.write(window["username_input"].get())
+        
 
 
         if success:
